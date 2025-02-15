291f9391e8d5e5c029599bde1dbe72f9406a7eeb
abhiram7352 <abhiramkumar8780@gmail.com>
2025-02-15 23:39:31 +0530

    [Abhiram] Added Testing Banking Transactions 🎉

---
+++ day05_java_regex/junit/BankAccount.java
@@ -0,0 +1,30 @@
+package day05_java_regex.junit;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            throw new IllegalArgumentException("Deposit amount must be positive");
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        } else {
+            return false; // Withdrawal fails due to insufficient funds
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
